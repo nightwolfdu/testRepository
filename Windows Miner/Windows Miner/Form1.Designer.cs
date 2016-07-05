5bbdbc418771c5707e6005578eb5d2ede551ddc4
nightwolfdu <nightwolf_du@mail.ru>
2016-07-05 15:45:57 +0300

    First commit on miner

---
+++ Windows Miner/Windows Miner/Form1.Designer.cs
@@ -0,0 +1,49 @@
+﻿namespace Windows_Miner
+{
+    partial class MinerForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MinerForm));
+            this.SuspendLayout();
+            // 
+            // MinerForm
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MinerForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MyMiner";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ Windows Miner/Windows Miner/GameLogic.cs
@@ -0,0 +1,20 @@
+﻿namespace Windows_Miner {
+    public  class GameLogic {
+    }
+
+    public class GameSquare {
+    }
+
+    public abstract class Field {
+        FieldType type;
+
+        public abstract void OnClickLogic();
+    }
+
+    public enum FieldType {
+        None = 0 ,
+        Empty = 1,
+        Bomb =2,
+        Number = 3
+    }
+}
